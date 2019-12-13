4b5dd41e3876b8651e4f8a020da54a1fe8b6c31c
tobias.heierli <tobias.heierli@gibwintra.ch>
2019-12-13 14:19:42 +0100

    added ConsoleCalculator.java

---
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/ConsoleCalculatorTest.java
@@ -0,0 +1,14 @@
+package ch.bbw.ConsoleCalculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConsoleCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
