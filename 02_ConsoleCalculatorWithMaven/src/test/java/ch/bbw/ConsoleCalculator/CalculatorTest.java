bcac2f55ec8fee386dc9f456e4e578bf1b16b21b
tobias.heierli <tobias.heierli@gibwintra.ch>
2019-12-13 14:23:53 +0100

    implemented CalculatorTest.java

--- 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
@@ -1,14 +1,34 @@
 package ch.bbw.ConsoleCalculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-class CalculatorTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest 
+{	
+	private Calculator testee;
+	
+	@Before
+	public void setUp() {
+		testee = new Calculator();
 	}
-
+    @Test
+    public void testSummeZweiPositive()
+    {
+        assertTrue(testee.summe(10, 20) == 30 );
+    }
+    @Test
+    public void testSummeZweiNegative()
+    {
+    	assertTrue(testee.summe(-10, -20) == -30 );
+    }
+    @Test
+    public void testSummeZweiNull()
+    {
+    	assertTrue(testee.summe(0, 0) == 0 );
+    }
 }
