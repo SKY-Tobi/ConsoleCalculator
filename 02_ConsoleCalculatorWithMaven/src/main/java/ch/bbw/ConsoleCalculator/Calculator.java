0534b7015239071096d1078129a773bde2280953
tobias.heierli <tobias.heierli@gibwintra.ch>
2019-12-13 14:22:54 +0100

    implemented Calculator.java

---
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/ConsoleCalculator/Calculator.java
@@ -0,0 +1,16 @@
+package ch.bbw.ConsoleCalculator;
+
+/**
+ * Calculator - kann zwei Zahlen addieren und zwei Zahlen subtrahieren
+ * 
+ * @author tobias.heierli
+ * @version 0.0.2
+ */
+public class Calculator {
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+	public int subtraktion(int value1, int value2) {
+		return value1 - value2;
+	}
+}
--- 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class ConsoleCalculatorTest {
+class CalculatorTest {
 
 	@Test
 	void test() {
