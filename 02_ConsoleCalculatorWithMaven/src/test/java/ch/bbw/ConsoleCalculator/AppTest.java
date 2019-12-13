7bb0685d9ac2074cec95a2de7a95830d065a49d5
tobias.heierli <tobias.heierli@gibwintra.ch>
2019-12-13 14:23:28 +0100

    implemented AppTest.java

--- 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/AppTest.java
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/ConsoleCalculator/AppTest.java
@@ -1,14 +1,35 @@
 package ch.bbw.ConsoleCalculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 class AppTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	public AppTest() {
+
+	}
+
+	@Nested
+	class Suite {
+
+		@Test
+		void testApp() {
+			JUnitCore junit = new JUnitCore();
+			Result result = junit.run(CalculatorTest.class);
+			if (result.wasSuccessful()) {
+
+			} else {
+				for (Failure failure : result.getFailures()) {
+					fail(failure.getException());
+				}
+			}
+		}
+
 	}
 
 }
