818621d9d459aa8ab6d2db322f83468f45220f43
tobias.heierli <tobias.heierli@gibwintra.ch>
2020-01-10 09:17:04 +0100

    Methode um das Endkapital mit dem Zinseszins zu berechnen implementiert

--- 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/ConsoleCalculator/Calculator.java
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/ConsoleCalculator/Calculator.java
@@ -1,10 +1,10 @@
 package ch.bbw.ConsoleCalculator;
 
 /**
- * Calculator - kann zwei Zahlen addieren und zwei Zahlen subtrahieren und zwei Zahlen dividieren und kann von einem double seinen Binärzahl berechnen
+ * Calculator - kann zwei Zahlen addieren und zwei Zahlen subtrahieren und zwei Zahlen dividieren und kann von einem double seinen Binärzahl berechnen. Das endkapital vom zinseszins kann auch berechnet werden.
  * 
  * @author tobias.heierli
- * @version 0.0.4
+ * @version 0.0.5
  */
 public class Calculator {
 	public int summe(int summand1, int summand2) {
@@ -18,5 +18,8 @@ public class Calculator {
 	}
 	public String doubleInBinär(double value) {
 		return Long.toBinaryString(Double.doubleToRawLongBits(value));
+	}
+	public double zinsesZins(double startkapital, double zinssatz, double jahre) {
+		return (double) Math.round(startkapital * Math.pow(1 + zinssatz / 100, jahre) * 100) / 100;
 	}
 }
